f5d038af566b6515d758569d8ec29b6ef4288cc8
L3tum <9307432+L3tum@users.noreply.github.com>
2019-12-08 15:49:07 +0100

    Update Program.cs

--- CPU-Benchmark-Database-Aggregator/Program.cs
+++ CPU-Benchmark-Database-Aggregator/Program.cs
@@ -40,6 +40,12 @@ namespace CPU_Benchmark_Database_Aggregator
 
 			foreach (var saveFile in saves)
 			{
+				// Skip automatically generated saves
+				if (saveFile.Contains(".automated."))
+				{
+					continue;
+				}
+
 				Console.Write('.');
 
 				try
